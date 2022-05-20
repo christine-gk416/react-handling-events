adbb10e97d3229cf8cbeec954eeec2660f3ca8a6
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-20 12:05:07 +0000

    Complete methods as props challenge

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import EventBinding from "./components/EventBinding";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+import MethodsAsPropsChild from "./components/MethodsAsPropsChild";
 
 function App() {
   return (
     <div className="App">
-      {/* <EventsClass />
-      <EventsFunctional />
-      <EventBinding />
-      <ConditionalRenderingClass/>
-      <ConditionalRenderingFunctional connected={false}/> */}
-      <NestingComponents/>
+      {/* <EventsClass /> */}
+      {/* <EventsFunctional /> */}
+      {/* <EventBinding /> */}
+      {/* <ConditionalRenderingClass/> */}
+      {/* <ConditionalRenderingFunctional connected={false}/> */}
+      {/* <NestingComponents/> */}
+      <MethodsAsPropsParent/>
+      
     </div>
   );
 }
