37637e236b6fe55f7e0949f39a68d44899c44ec8
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-20 20:46:02 +0000

    Complete componentDidMount lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
-import EventBinding from "./components/EventBinding";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
-import NestingComponents from "./components/NestingComponents";
-import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
-import MethodsAsPropsChild from "./components/MethodsAsPropsChild";
-import RenderingLists from "./components/RenderingLists";
-import Clock from "./components/Clock";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+// import EventBinding from "./components/EventBinding";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+// import NestingComponents from "./components/NestingComponents";
+// import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+// import MethodsAsPropsChild from "./components/MethodsAsPropsChild";
+// import RenderingLists from "./components/RenderingLists";
+// import Clock from "./components/Clock";
+import LifeCyclesCDM from "./components/LifeCyclesCDM";
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
       {/* <NestingComponents/> */}
       {/* <MethodsAsPropsParent/> */}
       {/* <RenderingLists/> */}
-      <Clock/>
+      {/* <Clock/> */}
+      <LifeCyclesCDM/>
     </div>
   );
 }
